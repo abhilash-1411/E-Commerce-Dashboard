55b8144e9e34332b1c195a36bca55425decd2928
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-05 12:58:02 +0530

    MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,19 @@
 const express=require('express');
-
+const mongoose=require('mongoose');
 const app=express();
+app.use(express.json());
+
+// Connect to MongoDB and fetch products
+const connectDB = async () => {
+        await mongoose.connect('mongodb://localhost:27017/E-comm');
+        const productSchema = new mongoose.Schema({  });
+        const Product = mongoose.model('products', productSchema);
+        const data=await Product.find();
+        console.log(data);
+};
+connectDB();
+
+
 
 
 app.get('/',(req,resp)=>{
