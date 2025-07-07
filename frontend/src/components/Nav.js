03361aca23f4e1ad1652bb5cb46146481b39769b
Abhilash Chandravanshi <abhilashchandravanshi.ab123@gmail.com>
2025-07-07 12:24:52 +0530

    Add logout functionality and update routes

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 
 const Nav=()=>{
    const auth=localStorage.getItem("user");
+    const navigate=useNavigate();
+
+   const logout=()=>{
+       localStorage.clear();
+       navigate("/signup");
+   }    
     return(
         <div>
             <ul className='nav-ul'>
@@ -10,7 +16,7 @@ const Nav=()=>{
                 <li><Link to="/add">Add Products</Link></li>
                 <li><Link to="/update">Update Products</Link></li>    
                 <li><Link to="/profile">Profile</Link></li>
-                <li>{auth ? <Link to="/logout">Logout</Link>: <Link to="/signup">Sign Up</Link>}</li>
+                <li>{auth ? <Link to="/signup" onClick={logout}>Logout</Link>: <Link to="/signup">Sign Up</Link>}</li>
 
             </ul>
         </div>
